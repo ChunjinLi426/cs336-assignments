ef274a60db6526fb3d4e920df5ecf63dc8d19a2e
ChunjinLi426 <licj24@mails.tsinghua.edu.cn>
2025-07-17 08:13:20 +0000

    Implement Embedding

--- cs336-a1/cs336_basics/transformer.py
+++ cs336-a1/cs336_basics/transformer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np 
+from einops import einsum 
 
 class Linear(nn.Module): 
     def __init__(self, in_features: int, out_features: int, device: torch.device | None = None, dtype: torch.dtype | None = None): 
@@ -21,4 +22,28 @@ class Linear(nn.Module):
     
     def forward(self, x: torch.Tensor) -> torch.Tensor: 
         return x @ self.weight.T
+
+class Embedding(nn.Module): 
+    def __init__(self, vocab_siz: int, d_model: int, device: torch.device | None = None, dtype: torch.dtype | None = None): 
+        super().__init__()
+        self.factory_kwargs = {"device": device, "dtype": dtype} 
+        self.vocab_siz = vocab_siz 
+        self.d_model = d_model
+        self.device = device
+        self.dtype = dtype 
+        
+        mean = 0
+        std = 1
+        w_init = torch.empty(vocab_siz, d_model)
+        nn.init.trunc_normal_(w_init, mean, std, -3, 3)
+        self.weight = nn.Parameter(w_init)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor: 
+        """
+        token_ids: (batch_size, sequence_length)
+        output: (batch_size, sequence_length, d_model)
+        """
+        return self.weight[token_ids]
+
+
     
